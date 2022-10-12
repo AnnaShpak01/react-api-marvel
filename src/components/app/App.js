0ac16246e8759f2692dd3ec3ad2928586e11fab8
Anna Shpak <elistana@i.ua>
2022-10-12 13:11:58 +0300

    project from classes to functions and hooks
    Reformat of project from classes to functions , using hooks and custom hooks

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -4,6 +4,8 @@ import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
 import ErrorBoundary from "../errorBoundary/ErrorBoundary";
+import AppBanner from "../appBanner/AppBanner";
+import ComicsList from "../comicsList/ComicsList";
 
 import decoration from "../../resources/img/vision.png";
 
@@ -18,7 +20,7 @@ const App = () => {
     <div className="app">
       <AppHeader />
       <main>
-        <ErrorBoundary>
+        {/* <ErrorBoundary>
           <RandomChar />
         </ErrorBoundary>
         {
@@ -31,7 +33,9 @@ const App = () => {
             </ErrorBoundary>
           </div>
         }
-        <img className="bg-decoration" src={decoration} alt="vision" />
+        <img className="bg-decoration" src={decoration} alt="vision" /> */}
+        <AppBanner />
+        <ComicsList />
       </main>
     </div>
   );
