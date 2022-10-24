f3095a34962d9bae147fa570af32b555a47e50ec
Anna Shpak <elistana@i.ua>
2022-10-24 22:43:04 +0300

    Formik and Yup
    form for input character name and Link to character page

--- src/components/pages/MainPage.js
+++ src/components/pages/MainPage.js
@@ -1,38 +1,42 @@
-import {useState} from 'react'
+import { useState } from "react";
 
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
+import CharSearchForm from "../charSearchForm/CharSearchForm";
 import CharInfo from "../charInfo/CharInfo";
 import ErrorBoundary from "../errorBoundary/ErrorBoundary";
 
 import decoration from "../../resources/img/vision.png";
 
-const MainPage = () =>{
+const MainPage = () => {
   const [selectedChar, setChar] = useState(null);
 
   const onCharSelected = (id) => {
     setChar(id);
   };
-    return (
-        <>
+  return (
+    <>
+      <ErrorBoundary>
+        <RandomChar />
+      </ErrorBoundary>
+      {
+        <div className="char__content">
           <ErrorBoundary>
-            <RandomChar />
+            <CharList onCharSelected={onCharSelected} />
           </ErrorBoundary>
-          {
-            <div className="char__content">
-            <ErrorBoundary>
-              <CharList onCharSelected={onCharSelected} />
-            </ErrorBoundary>
+          <div>
             <ErrorBoundary>
               <CharInfo charId={selectedChar} />
             </ErrorBoundary>
-            </div>
-          }
-          <img className="bg-decoration" src={decoration} alt="vision" /> 
-
-        </>
-    )
-} 
-
+            <ErrorBoundary>
+              <CharSearchForm />
+            </ErrorBoundary>
+          </div>
+        </div>
+      }
+      <img className="bg-decoration" src={decoration} alt="vision" />
+    </>
+  );
+};
 
 export default MainPage;
