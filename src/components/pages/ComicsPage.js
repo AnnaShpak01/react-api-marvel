00c01ec40bd9953aca737a837a2255cc431985fd
Anna Shpak <elistana@i.ua>
2022-10-25 11:06:41 +0300

    React-helmet
    SEO-optimization

--- src/components/pages/ComicsPage.js
+++ src/components/pages/ComicsPage.js
@@ -1,14 +1,19 @@
-import AppBanner from '../appBanner/AppBanner'
+import { Helmet } from "react-helmet";
+
+import AppBanner from "../appBanner/AppBanner";
 import ComicsList from "../comicsList/ComicsList";
 
-
 const ComicsPage = () => {
-    return (
-        <>
-            <AppBanner />
-            <ComicsList />
-        </>
-    )
-}
+  return (
+    <>
+      <Helmet>
+        <meta name="description" content="Page with list of our comics" />
+        <title>Comics Page</title>
+      </Helmet>
+      <AppBanner />
+      <ComicsList />
+    </>
+  );
+};
 
-export default ComicsPage
+export default ComicsPage;
