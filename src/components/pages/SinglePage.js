01d2790a5d0af52c8269d3f06bfcb62be1386aaf
Anna Shpak <elistana@i.ua>
2022-12-15 15:44:48 +0200

    delete old comment

--- src/components/pages/SinglePage.js
+++ src/components/pages/SinglePage.js
@@ -4,7 +4,6 @@ import { useState, useEffect } from "react";
 import useMarvelService from "../../services/MarvelService";
 import AppBanner from "../appBanner/AppBanner";
 import setContent from "../../utils/setContent";
-// Хотелось бы вынести функцию по загрузке данных как отдельный аргумент
 // Но тогда мы потеряем связь со стэйтами загрузки и ошибки
 // А если вынесем их все в App.js - то они будут одни на все страницы
 
